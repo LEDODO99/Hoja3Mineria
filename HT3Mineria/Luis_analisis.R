fa6f3446dd6bc7f1e8c2c25d91b1ec0e10d36542
Luis <ledodo99@hotmail.com>
2020-03-19 17:33:32 -0600

    Miscosas

--- HT3Mineria/Luis_analisis.R
+++ HT3Mineria/Luis_analisis.R
@@ -1,4 +1,13 @@
+library(cluster) #Para calcular la silueta
+library(e1071)#para cmeans
+library(mclust) #mixtures of gaussians
+library(fpc) #para hacer el plotcluster
+library(NbClust) #Para determinar el n?mero de clusters ?ptimo
+library(factoextra) #Para hacer gr?ficos bonitos de clustering
+
+
 a=read.csv("sample_submission.csv")
 testData=read.csv("test.csv")
 trainData=read.csv("train.csv")
 trainData["PoolQC"]
+
